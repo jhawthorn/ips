79d1b9840772bee46f9fb29cf7a8463b9219a31b
John Hawthorn <john@hawthorn.email>
2026-03-17 18:03:32 -0700

    Initial commit

---
+++ lib/ips.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ips/version"
+
+module Ips
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ips/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Ips
+  VERSION = "0.1.0"
+end
